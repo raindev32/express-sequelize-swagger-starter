07fdfd8fbb3787450b5ac298070f3666812fb252
jhonyrain32 <jhonylau95@gmail.com>
2020-12-30 00:18:33 +0700

    add passport and fix config.

--- config/project.config.js
+++ config/project.config.js
@@ -26,6 +26,8 @@ const config = {
    server_port: process.env.LOCAL_PORT || 5555,
    api_host: process.API_HOST || 'localhost',
    api_port: process.NODE_PORT || process.env.API_PORT || 3002,
+   api_version: process.env.API_VERSION,
+   api_prefix: process.env.API_PREFIX,
 
    // database configuration 
    db_host: process.env.DB_HOST || '127.0.0.1',
@@ -36,7 +38,15 @@ const config = {
    db_dialect: process.env.DB_DIALECT,
    db_logging: process.env.DB_LOGGING,
 
-   docs_host: process.env.DOCS_HOST || 'localhost:5000'
+   auth_by: process.env.AUTH_BY,
+   auth_secret: process.env.AUTH_SECRET,
+   auth_expire: process.env.AUTH_EXPIRE,
+   auth_cookie_secret: process.env.AUTH_COOKIE_SECRET,
+   auth_cookie_name: process.env.AUTH_COOKIE_NAME,
+
+   docs_host: process.env.DOCS_HOST || 'localhost:5000',
+   docs_path: process.env.DOCS_PATH || '/',
+
 }
 
 debug(`Build default configuration [${config.env}]`) // create debug for info app

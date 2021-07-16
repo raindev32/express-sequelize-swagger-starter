014db4c506627229878ea269afe74acacaa03f62
jhonyrain32 <jhonylau95@gmail.com>
2021-07-16 20:29:09 +0700

    add services user

--- src/services/users/usersService.js
+++ src/services/users/usersService.js
@@ -5,58 +5,59 @@ const User = db.users
 const Role = db.roles
 
 User.belongsTo(Role, {
-   as: 'roles',
-   foreignKey: 'role_id',
-   sourceKey: 'role_id'
+  as: 'roles',
+  foreignKey: 'role_id',
+  sourceKey: 'role_id'
 })
 
 export const requireAuth = passport.authenticate('jwt', { session: false })
 
 export const getDataAllUser = () => {
-   return User.findAll()
+  return User.findAll()
 }
 
 export const getUserById = (id) => {
-   return User.findOne({
-      include: ['roles'],
-      where: {
-         id: id
-      }
-   })
+  return User.findOne({
+    include: ['roles'],
+    where: {
+      id
+    }
+  })
 }
 
 export const createData = (data) => {
-   return User.create({
-      name: data.name,
-      age: data.age,
-      role_id: data.role_id
-   })
+  return User.create({
+    name: data.name,
+    age: data.age,
+    role_id: data.role_id,
+    photo: data.file.path
+  })
 }
 
 export const dataExists = async (id) => {
-   const exists = await getUserById(id)
-   if (exists === null) {
-      return false
-   }
-   return true
+  const exists = await getUserById(id)
+  if (exists === null) {
+    return false
+  }
+  return true
 }
 
 exports.updateData = (id, data) => {
-   return User.update({
-      name: data.name,
-      age: data.age,
-      role_id: data.role_id
-   }, {
-      where: {
-         id
-      }
-   })
+  return User.update({
+    name: data.name,
+    age: data.age,
+    role_id: data.role_id
+  }, {
+    where: {
+      id
+    }
+  })
 }
 
 exports.deleteData = (id) => {
-   return User.destroy({
-      where: {
-         id
-      }
-   })
+  return User.destroy({
+    where: {
+      id
+    }
+  })
 }

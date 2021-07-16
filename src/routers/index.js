912d8edb23a87bf463f1b860af2be94b7404ea4e
jhonyrain32 <jhonylau95@gmail.com>
2021-07-16 20:30:51 +0700

    add routers index

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,10 +3,10 @@ import userRouter from './v1/user/userRouter'
 import roleRouter from './v1/role/roleRouter'
 
 
-function routes(app) {
-   app.use(authRouter)
-   app.use(userRouter)
-   app.use(roleRouter)
+function routes (app) {
+  app.use(authRouter)
+  app.use(userRouter)
+  app.use(roleRouter)
 }
 
 export default routes
